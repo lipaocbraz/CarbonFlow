eaad6d288a2e03964add67338194881cd7c4d1cc
Daniel S. <danielbarbosa27@icloud.com>
2026-05-12 18:09:37 -0300

    Base para desenvolvimento do site

---
+++ backend/src/main/java/com/carbonflow/CarbonflowApplication.java
@@ -0,0 +1,11 @@
+package com.carbonflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbonflowApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CarbonflowApplication.class, args);
+    }
+}
